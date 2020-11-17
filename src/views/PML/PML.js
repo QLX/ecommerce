44b9027bdd277c438e3fc55f34240418727a542e
Reed  Murphy <reedmurphy@Reeds-MBP.home>
2020-11-17 17:01:41 -0500

    Fixing PML look and feel

--- src/views/PML/PML.js
+++ src/views/PML/PML.js
@@ -4,7 +4,7 @@ import XLSX from 'xlsx';
 import FileSaver from "file-saver";
 
 import Checkbox from '@material-ui/core/Checkbox';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles, createMuiTheme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -12,11 +12,13 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TablePagination from '@material-ui/core/TablePagination';
+import Button from '@material-ui/core/Button';
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
 
 import TextField from '@material-ui/core/TextField';
 
@@ -51,10 +53,14 @@ const StyledTableRow = withStyles(() => ({
 
 const useStyles = makeStyles({
   buttonGroup: {
-    background: "linear-gradient(45deg, #ea4845, #fea320 70%)",
-    padding: "5px",
+    // background: "linear-gradient(45deg, #ea4845, #fea320 70%)",
+    padding: "10px",
+    color: "white",
+    border: "none"
   }
 });
+
+
 
 export default function TableFromScratch() {
   const classes = useStyles();
@@ -217,106 +223,113 @@ export default function TableFromScratch() {
 
   return (
     <div className="container">
-      
-      <TableContainer>
+      <Card style={{paddingLeft: "40px"}}>
+        <CardHeader>
           <Grid container spacing={3}>
             <Grid item>
               <Typography variant="h4">Product Master List</Typography>
             </Grid>
             <Grid item>
-              <ButtonGroup className={classes.buttonGroup} size="medium">
-                <Button onClick={() => exportToExcel()}>Export to Excel</Button>
-                <Button>Add to Repricing</Button>
-              </ButtonGroup>
+              <button onClick={() => exportToExcel()} className={classes.buttonGroup} style={{ backgroundColor: "#ea4845", borderRadius: "5px 0px 0px 5px" }}>
+                <Typography>Export to Excel</Typography></button>
+              <button className={classes.buttonGroup} style={{ backgroundColor: "#ec5e5b", borderRadius: "0px 5px 5px 0px" }}>
+                <Typography>Add to Repricing</Typography></button>
             </Grid>
           </Grid>
-        <Table>
-          <TableHead >
-            <StyledTableRow >
-              <StyledTableCell></StyledTableCell>
-              {data.keys.map(title => title === "PackagingFactor" || title === "Variable_Margin" ?
-                <EditibleTableCell>{title}</EditibleTableCell> : <StyledTableCell>{title}</StyledTableCell>)}
-            </StyledTableRow>
-          </TableHead>
-          <TableBody>
-            {
-              data.currentData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((item, index) => (
-                  <StyledTableRow key={item.UPC}>
-                    <StyledTableCell><Checkbox /></StyledTableCell>
-                    <StyledTableCell>{item.UPC}</StyledTableCell>
-                    <StyledTableCell>{item.ASIN}</StyledTableCell>
-                    <StyledTableCell>{item.SKU}</StyledTableCell>
-                    <StyledTableCell>{item.Title}</StyledTableCell>
-                    <StyledTableCell>{item.Brand}</StyledTableCell>
-                    <StyledTableCell>{item.ProductCost}</StyledTableCell>
-                    <StyledTableCell>{item.unitWeight}</StyledTableCell>
-                    <StyledTableCell>{item.ShippingCost}</StyledTableCell>
-                    <StyledTableCell>{item.ReferralFee}</StyledTableCell>
-                    <StyledTableCell onClick={() => onEdit({ UPC: item.UPC, columnName: "PackagingFactor" })}>
-                      {
-                        inEditMode.status && inEditMode.rowKey === item.UPC && inEditMode.columnName === "PackagingFactor" ? (
-                          <div>
-                            <TextField value={value} placeholder={item.PackagingFactor}
-                              onChange={(event) => changeValue(event.target.value)}
-                              onKeyPress={(event) => keyPress(event, { UPC: item.UPC, columnName: "PackagingFactor" })}
-                              autoFocus
-                            />
-                          </div>
-                        ) : (
-                            item.PackagingFactor
-                          )
-                      }
-                    </StyledTableCell>
-                    <StyledTableCell>{item.BuyBoxPrice}</StyledTableCell>
-                    <StyledTableCell style={conditionalStyle("TotalProductCost", item, index)}>{item.TotalProductCost}</StyledTableCell>
-                    <StyledTableCell style={conditionalStyle("Profit", item, index)}>{item.Profit}</StyledTableCell>
-                    <StyledTableCell style={conditionalStyle("Margin", item, index)}>{item.Margin}</StyledTableCell>
-                    <StyledTableCell style={conditionalStyle("ROI", item, index)}>{item.ROI}</StyledTableCell>
-                    <StyledTableCell onClick={() => onEdit({ UPC: item.UPC, columnName: "Variable_Margin" })}>
-                      {
-                        inEditMode.status && inEditMode.rowKey === item.UPC && inEditMode.columnName === "Variable_Margin" ? (
-                          <div>
-                            <TextField value={value} placeholder={item.Variable_Margin}
-                              onChange={(event) => changeValue(event.target.value)}
-                              onKeyPress={(event) => keyPress(event, { UPC: item.UPC, columnName: "Variable_Margin" })}
-                              autoFocus
-                            />
-                          </div>
-                        ) : (
-                            item.Variable_Margin
-                          )
-                      }
-                    </StyledTableCell>
-                    <StyledTableCell style={conditionalStyle("New_Price_Factor", item, index)}>{item.New_Price_Factor}</StyledTableCell>
-                    <StyledTableCell style={conditionalStyle("New_Price", item, index)}>{item.New_Price}</StyledTableCell>
-                    <StyledTableCell style={conditionalStyle("New_Referral_Fee", item, index)}>{item.New_Referral_Fee}</StyledTableCell>
-                    <StyledTableCell style={conditionalStyle("New_Profit", item, index)}>{item.New_Profit}</StyledTableCell>
-                    <StyledTableCell style={conditionalStyle("New_Margin", item, index)}>{item.New_Margin}</StyledTableCell>
-                    <StyledTableCell style={conditionalStyle("New_ROI", item, index)}>{item.New_ROI}</StyledTableCell>
-                    <StyledTableCell style={conditionalStyle("Min_Price", item, index)}>{item.Min_Price}</StyledTableCell>
-                    <StyledTableCell style={conditionalStyle("Max_Price", item, index)}>{item.Max_Price}</StyledTableCell>
-                    <StyledTableCell>{item.Match_Buy_Box}</StyledTableCell>
-                    <StyledTableCell>{item.Channel}</StyledTableCell>
-                    <StyledTableCell>{item.BSR}</StyledTableCell>
-                    <StyledTableCell>{item.Quantity}</StyledTableCell>
-                    <StyledTableCell>{item.Rating}</StyledTableCell>
-                  </StyledTableRow>
-                ))
-            }
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
-        component="div"
-        count={data.currentData.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-        style={{ backgroundColor: "#3f51b5", borderRadius: "0px 0px 10px 10px", color: "white" }}
-      />
+        </CardHeader>
+      </Card>
+      <Card style={{padding: "15px"}}>
+        <CardHeader>
+          <TableContainer style={{ borderRadius: "10px 10px 0px 0px" }}>
+            <Table>
+              <TableHead >
+                <StyledTableRow >
+                  <StyledTableCell></StyledTableCell>
+                  {data.keys.map(title => title === "PackagingFactor" || title === "Variable_Margin" ?
+                    <EditibleTableCell>{title}</EditibleTableCell> : <StyledTableCell>{title}</StyledTableCell>)}
+                </StyledTableRow>
+              </TableHead>
+              <TableBody>
+                {
+                  data.currentData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((item, index) => (
+                      <StyledTableRow key={item.UPC}>
+                        <StyledTableCell><Checkbox /></StyledTableCell>
+                        <StyledTableCell>{item.UPC}</StyledTableCell>
+                        <StyledTableCell>{item.ASIN}</StyledTableCell>
+                        <StyledTableCell>{item.SKU}</StyledTableCell>
+                        <StyledTableCell>{item.Title}</StyledTableCell>
+                        <StyledTableCell>{item.Brand}</StyledTableCell>
+                        <StyledTableCell>{item.ProductCost}</StyledTableCell>
+                        <StyledTableCell>{item.unitWeight}</StyledTableCell>
+                        <StyledTableCell>{item.ShippingCost}</StyledTableCell>
+                        <StyledTableCell>{item.ReferralFee}</StyledTableCell>
+                        <StyledTableCell onClick={() => onEdit({ UPC: item.UPC, columnName: "PackagingFactor" })}>
+                          {
+                            inEditMode.status && inEditMode.rowKey === item.UPC && inEditMode.columnName === "PackagingFactor" ? (
+                              <div>
+                                <TextField value={value} placeholder={item.PackagingFactor}
+                                  onChange={(event) => changeValue(event.target.value)}
+                                  onKeyPress={(event) => keyPress(event, { UPC: item.UPC, columnName: "PackagingFactor" })}
+                                  autoFocus
+                                />
+                              </div>
+                            ) : (
+                                item.PackagingFactor
+                              )
+                          }
+                        </StyledTableCell>
+                        <StyledTableCell>{item.BuyBoxPrice}</StyledTableCell>
+                        <StyledTableCell style={conditionalStyle("TotalProductCost", item, index)}>{item.TotalProductCost}</StyledTableCell>
+                        <StyledTableCell style={conditionalStyle("Profit", item, index)}>{item.Profit}</StyledTableCell>
+                        <StyledTableCell style={conditionalStyle("Margin", item, index)}>{item.Margin}</StyledTableCell>
+                        <StyledTableCell style={conditionalStyle("ROI", item, index)}>{item.ROI}</StyledTableCell>
+                        <StyledTableCell onClick={() => onEdit({ UPC: item.UPC, columnName: "Variable_Margin" })}>
+                          {
+                            inEditMode.status && inEditMode.rowKey === item.UPC && inEditMode.columnName === "Variable_Margin" ? (
+                              <div>
+                                <TextField value={value} placeholder={item.Variable_Margin}
+                                  onChange={(event) => changeValue(event.target.value)}
+                                  onKeyPress={(event) => keyPress(event, { UPC: item.UPC, columnName: "Variable_Margin" })}
+                                  autoFocus
+                                />
+                              </div>
+                            ) : (
+                                item.Variable_Margin
+                              )
+                          }
+                        </StyledTableCell>
+                        <StyledTableCell style={conditionalStyle("New_Price_Factor", item, index)}>{item.New_Price_Factor}</StyledTableCell>
+                        <StyledTableCell style={conditionalStyle("New_Price", item, index)}>{item.New_Price}</StyledTableCell>
+                        <StyledTableCell style={conditionalStyle("New_Referral_Fee", item, index)}>{item.New_Referral_Fee}</StyledTableCell>
+                        <StyledTableCell style={conditionalStyle("New_Profit", item, index)}>{item.New_Profit}</StyledTableCell>
+                        <StyledTableCell style={conditionalStyle("New_Margin", item, index)}>{item.New_Margin}</StyledTableCell>
+                        <StyledTableCell style={conditionalStyle("New_ROI", item, index)}>{item.New_ROI}</StyledTableCell>
+                        <StyledTableCell style={conditionalStyle("Min_Price", item, index)}>{item.Min_Price}</StyledTableCell>
+                        <StyledTableCell style={conditionalStyle("Max_Price", item, index)}>{item.Max_Price}</StyledTableCell>
+                        <StyledTableCell>{item.Match_Buy_Box}</StyledTableCell>
+                        <StyledTableCell>{item.Channel}</StyledTableCell>
+                        <StyledTableCell>{item.BSR}</StyledTableCell>
+                        <StyledTableCell>{item.Quantity}</StyledTableCell>
+                        <StyledTableCell>{item.Rating}</StyledTableCell>
+                      </StyledTableRow>
+                    ))
+                }
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={data.currentData.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+            style={{ backgroundColor: "#3f51b5", borderRadius: "0px 0px 10px 10px", color: "white" }}
+          />
+        </CardHeader>
+      </Card>
     </div>
   )
 }
